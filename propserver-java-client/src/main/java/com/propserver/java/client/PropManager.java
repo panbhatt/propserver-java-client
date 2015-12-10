aa2a65c2f410e1b909d64596bdb197248b687239
panbhatt <panbhatt@gmail.com>
2015-12-10 16:49:12 -0500

    Added refresh() method and Changed the README.md
    Added refresh() method and Changed the README.md

--- propserver-java-client/src/main/java/com/propserver/java/client/PropManager.java
+++ propserver-java-client/src/main/java/com/propserver/java/client/PropManager.java
@@ -15,119 +15,135 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-
-
 /**
  *
  * @author PBhatt
  */
 public final class PropManager {
-    
-    private static PropManager propManager  = null  ; 
-    private String configUrl, project, env, release, fullURL; 
+
+    private static PropManager propManager = null;
+    private String configUrl, project, env, release, fullURL;
     private JSONObject parsedJson;
-    
+
     public static void main(String[] args) {
-        
-        PropManager prpManager = PropManager.getInstance("http://localhost:3000", "VisaCheckout", "DEV", "0.0") ;
-        
+
+        PropManager prpManager = PropManager.getInstance("http://localhost:3000", "VisaCheckout", "DEV", "0.0");
+
     }
-    
-    
+
     // Made the constructor private. 
     private PropManager(String configUrl, String project, String env, String release) {
-        
-        this.configUrl = configUrl; 
-        this.project = project; 
-        this.env = env; 
-        this.release = release; 
-        
-        this.fullURL = configUrl+"/projects/"+project+"/env/"+env+"/release/"+release+"?flatten=true";
+
+        this.configUrl = configUrl;
+        this.project = project;
+        this.env = env;
+        this.release = release;
+
+        this.fullURL = configUrl + "/projects/" + project + "/env/" + env + "/release/" + release + "?flatten=true";
     }
-    
+
     /**
-     * This function is responsible for returning the Single Instance of the Prop Server. 
+     * This function is responsible for returning the Single Instance of the
+     * Prop Server.
+     *
      * @param configUrl
      * @param project
      * @param release
      * @param env
-     * @return 
+     * @return
      */
     public final synchronized static PropManager getInstance(String configUrl, String project, String env, String release) {
-        
-        if(propManager == null) {
+
+        if (propManager == null) {
             propManager = new PropManager(configUrl, project, env, release);
             propManager.initialize();
         }
-        return propManager; 
+        return propManager;
     }
-    
+
     /**
-     * This is the function which will be responsible for returning a specific Key of an Object. 
+     * This is the function which will be responsible for returning a specific
+     * Key of an Object.
+     *
      * @param keyName
-     * @return 
+     * @return
      */
     public final String get(String keyName) {
-        Object value =  parsedJson.get(keyName); 
-        if(value != null)
-            return value.toString(); 
-        else 
-            return null ;
+        if (parsedJson instanceof JSONObject) {
+            Object value = parsedJson.get(keyName);
+            if (value != null) {
+                return value.toString();
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
     }
-    
-    public final String getAllKeys() {
-        
-        if(parsedJson instanceof JSONObject)
-            return parsedJson.toString(); 
-        else 
-         return  "" ;
-    }
-    
-    
+
     /**
-     * This is the main method which is responsible 
+     * This function will return all the keys as STRING. 
+     * @return 
      */
-    private void initialize()  {    
-         
+    public final String getAllKeys() {
+
+        if (parsedJson instanceof JSONObject) {
+            return parsedJson.toString();
+        } else {
+            return "";
+        }
+    }
+
+    /**
+     * THis function refresh the Set of properties for the project afresh.
+     *
+     * @return
+     */
+    public final void refresh() {
+        if(propManager != null)
+            propManager.initialize();
+    }
+
+    /**
+     * This is the main method which is responsible
+     */
+    private void initialize() {
+
         try {
             URL propServerUrl = new URL(this.fullURL);
             HttpURLConnection httpConn = (HttpURLConnection) propServerUrl.openConnection();
             httpConn.setRequestMethod("GET");
             int responseCode = httpConn.getResponseCode();
-            if(responseCode == 200) {
-                
+            if (responseCode == 200) {
+
                 BufferedReader in = new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
-		String inputLine;
+                String inputLine;
                 JSONParser parser = new JSONParser();
-		StringBuilder response = new StringBuilder();
-                
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
-                
-		System.out.println(response.toString());
-                
-                parsedJson =  (JSONObject)parser.parse(response.toString()) ;
-                
+                StringBuilder response = new StringBuilder();
+
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                System.out.println(response.toString());
+
+                parsedJson = (JSONObject) parser.parse(response.toString());
+
             } else {
                 System.err.println("The PropServer has responded with an Error Code. Please see the details. Error Code = " + responseCode);
             }
-        } 
-        catch ( MalformedURLException ex) {
-            System.err.println("URL Specified is not correct. " +this. fullURL ); 
+        } catch (MalformedURLException ex) {
+            System.err.println("URL Specified is not correct. " + this.fullURL);
             ex.printStackTrace();
-        }
-        catch ( IOException ioEx) {
-            System.err.println("IO Exception has been occrred while connecting to the URL :  " +this. fullURL ); 
+        } catch (IOException ioEx) {
+            System.err.println("IO Exception has been occrred while connecting to the URL :  " + this.fullURL);
             ioEx.printStackTrace();
-        }
-        catch (ParseException parseEx) {
-            System.err.println("Parse Exception has been occrred while parsing Data from the URL  :  " +this. fullURL );  
+        } catch (ParseException parseEx) {
+            System.err.println("Parse Exception has been occrred while parsing Data from the URL  :  " + this.fullURL);
             parseEx.printStackTrace();
         }
-        
+
     }
-            
-    
+
 }
